757a28ae0327c963e40ada8d00f4e7247aed14dd
Adrian Szymanski <adrian.szymanski89@gmail.com>
2019-06-03 19:32:13 +0200

    New portfolio updated

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+  $('.topRow, .mainRow, .bottomBar, .copyright, #info, .arrowDown, .mainContact').css('display', 'none').fadeIn(3000);
+});
+
+$(document).ready(function () {
+  $('.description, .mainWork, .circle, .leftBar').css('display', 'none').fadeIn(1500);
+});
+
+$(document).ready(function () {
+  $('.parallelogram').css('display', 'none').fadeIn(900);
+});
+
+$(document).ready(function () {
+  $('.parallelogram').animate({width: '70%', height: '10%', top: '410px'}, 2000);
+}); 
+
+$(document).ready(function () {
+  $('.circle').animate({left: '10px'}, 1000);
+});
